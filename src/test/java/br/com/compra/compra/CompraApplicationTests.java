0903515c78a88387fabc99d43f0e9816c8d7649b
romario <romario.ifg@gmail.com>
2021-04-13 14:56:51 -0300

    primeiri commit

---
+++ src/test/java/br/com/compra/compra/CompraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compra.compra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
